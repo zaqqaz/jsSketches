ac11b74b4ff39259daff6e6afae3dc5a527e9207
Denis Artyuhovich <i@denis.by>
2018-08-14 06:54:05 +0300

    Binary search. Iterative solution

--- Algorithms/Search/BinarySearch.spec.ts
+++ Algorithms/Search/BinarySearch.spec.ts
@@ -1,9 +1,17 @@
-import { binarySearch } from "./BinarySearch";
+import { binarySearch, binarySearchIterative } from "./BinarySearch";
 
 describe("Binary search", () => {
     it("should return index where element occur", () => {
         expect(binarySearch(3, [1, 2, 3, 4, 5, 12, 16, 119])).toBe(2);
         expect(binarySearch(12, [1, 2, 3, 4, 5, 12, 16, 119])).toBe(5);
         expect(binarySearch(119, [1, 2, 3, 4, 5, 12, 16, 119])).toBe(7);
+        expect(binarySearch(119, [])).toBe(undefined);
+    })
+
+    it("should return index where element occur", () => {
+        expect(binarySearchIterative(3, [1, 2, 3, 4, 5, 12, 16, 119])).toBe(2);
+        expect(binarySearchIterative(12, [1, 2, 3, 4, 5, 12, 16, 119])).toBe(5);
+        expect(binarySearchIterative(119, [1, 2, 3, 4, 5, 12, 16, 119])).toBe(7);
+        expect(binarySearchIterative(119, [])).toBe(undefined);
     })
 });
--- Algorithms/Search/BinarySearch.tsx
+++ Algorithms/Search/BinarySearch.tsx
@@ -4,7 +4,11 @@ export function binarySearch(
     start: number = 0,
     end: number = sortedArray.length - 1
 ): number | undefined {
-    const half = Math.floor( (end - start) / 2) + start;
+    if(end < start) {
+        return;
+    }
+
+    const half = Math.floor((end - start) / 2) + start;
 
     if (value === sortedArray[half]) {
         return half;
@@ -14,5 +18,30 @@ export function binarySearch(
         return binarySearch(value, sortedArray, half + 1, end);
     } else {
         return binarySearch(value, sortedArray, start, half - 1);
+    }
+}
+
+export function binarySearchIterative(
+    value: number,
+    sortedArray: number[],
+    start: number = 0,
+    end: number = sortedArray.length - 1
+): number | undefined {
+    while(true) {
+        if (end < start) {
+            return;
+        }
+
+        const half = Math.floor((end - start) / 2) + start;
+
+        if (value === sortedArray[half]) {
+            return half;
+        }
+
+        if (value > sortedArray[half]) {
+            start = half + 1;
+        } else {
+            end = half - 1;
+        }
     }
 }
