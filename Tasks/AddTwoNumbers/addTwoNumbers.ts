87a47a28e3228212372dc0bdc28396f75e1ee475
Denis Artyuhovich <i@denis.by>
2018-10-20 16:47:21 +0300

    Simplified solution with two numbers

--- Tasks/AddTwoNumbers/addTwoNumbers.ts
+++ Tasks/AddTwoNumbers/addTwoNumbers.ts
@@ -1,68 +1,30 @@
 export class ListNode {
-    public val: number| undefined;
+    public val: number | undefined;
     public next: ListNode | undefined;
 }
 
-function getValues(root: ListNode) {
-    let next: ListNode | undefined = root;
-    const result = [];
-
-    while (next) {
-        result.push(next.val);
-        next = next.next;
-    }
-
-    return result;
-}
-
-function addValue(arr: number[], value: number, currentIndex:number): number[] {
-    value = (arr[currentIndex] || 0) + value;
-
-    if (value > 9) {
-        arr[currentIndex] = value % 10;
-        return addValue(arr, 1, currentIndex - 1);
-    }
-
-    if(currentIndex < 0) {
-        arr = [value, ...arr];
-    } else  {
-       arr[currentIndex] = value;
-    }
-
-    return arr;
-}
-
-export function addTwoNumbers(n1: ListNode, n2: ListNode) {
-    const v1 = getValues(n1).reverse();
-    const v2 = getValues(n2).reverse();
-
-    let result: number[] = [];
-    const max = Math.max(v1.length, v2.length);
-    const v1LengthDiff = max - v1.length;
-    const v2LengthDiff = max - v2.length;
-
-    for(let i = 0; i < max; i++) {
-        let sum = 0;
-        if(i >= v1LengthDiff) {
-            sum += v1[i - v1LengthDiff]!;
-        }
-        if(i >= v2LengthDiff) {
-            sum += v2[i - v2LengthDiff]!;
-        }
-
-        result = addValue(result, sum, result.length);
-    }
-
+export function addTwoNumbers(n1: ListNode | undefined, n2: ListNode | undefined) {
     const root = new ListNode();
-    let next = root;
+    let current = root;
+    let curring = 0;
 
-    while(result.length) {
-        next.val = +result.pop()!;
+    while (n1 || n2) {
+        const v1 = n1 ? n1.val : 0;
+        const v2 = n2 ? n2.val : 0;
+        const sum = curring + v1! + v2!;
+        curring = Math.floor(sum / 10);
+        current.val = sum % 10;
+        n1 = n1 ? n1.next : undefined;
+        n2 = n2 ? n2.next : undefined;
 
-        if(result.length) {
-            next.next = new ListNode();
-            next = next.next;
+        if (n1 || n2 || curring) {
+            current.next = new ListNode();
+            current = current.next;
         }
+    }
+
+    if (curring) {
+        current.val = curring;
     }
 
     return root;
